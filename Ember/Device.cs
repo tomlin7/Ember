946727ef5d8f945f8f36387892e523bbae5836d2
billyeatcookies <billydevbusiness@gmail.com>
2021-01-29 10:35:23 +0000

    ✨ Load models from external babylon files

--- Ember/Device.cs
+++ Ember/Device.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Media.Imaging;
 using SharpDX;
 
@@ -176,6 +178,70 @@ namespace Ember
                     DrawBLine(pixelC, pixelA);
                 }
             }
+        }
+        
+        // Loading the JSON file in an asynchronous manner
+        public async Task<Mesh[]> LoadJSONFileAsync(string fileName)
+        {
+            var meshes = new List<Mesh>();
+            var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(fileName);
+            var data = await Windows.Storage.FileIO.ReadTextAsync(file);
+            dynamic jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject(data);
+
+            for (var meshIndex = 0; meshIndex < jsonObject.meshes.Count; meshIndex++)
+            {
+                var verticesArray = jsonObject.meshes[meshIndex].vertices;
+                // Faces
+                var indicesArray = jsonObject.meshes[meshIndex].indices;
+
+                var uvCount = jsonObject.meshes[meshIndex].uvCount.Value;
+                var verticesStep = 1;
+
+                // Depending of the number of texture's coordinates per vertex
+                // we're jumping in the vertices array  by 6, 8 & 10 windows frame
+                switch ((int)uvCount)
+                {
+                    case 0:
+                        verticesStep = 6;
+                        break;
+                    case 1:
+                        verticesStep = 8;
+                        break;
+                    case 2:
+                        verticesStep = 10;
+                        break;
+                }
+
+                // the number of interesting vertices information for us
+                var verticesCount = verticesArray.Count / verticesStep;
+                // number of faces is logically the size of the array divided by 3 (A, B, C)
+                var facesCount = indicesArray.Count / 3;
+                var mesh = new Mesh(jsonObject.meshes[meshIndex].name.Value, verticesCount, facesCount);
+
+                // Filling the Vertices array of our mesh first
+                for (var index = 0; index < verticesCount; index++)
+                {
+                    var x = (float)verticesArray[index * verticesStep].Value;
+                    var y = (float)verticesArray[index * verticesStep + 1].Value;
+                    var z = (float)verticesArray[index * verticesStep + 2].Value;
+                    mesh.Vertices[index] = new Vector3(x, y, z);
+                }
+
+                // Then filling the Faces array
+                for (var index = 0; index < facesCount; index++)
+                {
+                    var a = (int)indicesArray[index * 3].Value;
+                    var b = (int)indicesArray[index * 3 + 1].Value;
+                    var c = (int)indicesArray[index * 3 + 2].Value;
+                    mesh.Faces[index] = new Face { A = a, B = b, C = c };
+                }
+
+                // Getting the position you've set in Blender
+                var position = jsonObject.meshes[meshIndex].position;
+                mesh.Position = new Vector3((float)position[0].Value, (float)position[1].Value, (float)position[2].Value);
+                meshes.Add(mesh);
+            }
+            return meshes.ToArray();
         }
     }
 }
--- Ember/MainPage.xaml.cs
+++ Ember/MainPage.xaml.cs
@@ -24,60 +24,97 @@ namespace Ember
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // private Device device;
+        // Mesh _mesh = new Mesh("Cube", 8, 12);
+        // Camera _camera = new Camera();
+        //
+        // private void Page_Loaded(object sender, RoutedEventArgs e)
+        // {
+        //     // Choose the back buffer resolution here
+        //     WriteableBitmap bmp = new WriteableBitmap(640, 480);
+        //
+        //     device = new Device(bmp);
+        //
+        //     // Our Image XAML control
+        //     frontBuffer.Source = bmp;
+        //
+        //     _mesh.Vertices[0] = new Vector3(-1, 1, 1);
+        //     _mesh.Vertices[1] = new Vector3(1, 1, 1);
+        //     _mesh.Vertices[2] = new Vector3(-1, -1, 1);
+        //     _mesh.Vertices[3] = new Vector3(1, -1, 1);
+        //     _mesh.Vertices[4] = new Vector3(-1, 1, -1);
+        //     _mesh.Vertices[5] = new Vector3(1, 1, -1);
+        //     _mesh.Vertices[6] = new Vector3(1, -1, -1);
+        //     _mesh.Vertices[7] = new Vector3(-1, -1, -1);
+        //     
+        //     _mesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
+        //     _mesh.Faces[1] = new Face { A = 1, B = 2, C = 3 };
+        //     _mesh.Faces[2] = new Face { A = 1, B = 3, C = 6 };
+        //     _mesh.Faces[3] = new Face { A = 1, B = 5, C = 6 };
+        //     _mesh.Faces[4] = new Face { A = 0, B = 1, C = 4 };
+        //     _mesh.Faces[5] = new Face { A = 1, B = 4, C = 5 };
+        //     
+        //     _mesh.Faces[6] = new Face { A = 2, B = 3, C = 7 };
+        //     _mesh.Faces[7] = new Face { A = 3, B = 6, C = 7 };
+        //     _mesh.Faces[8] = new Face { A = 0, B = 2, C = 7 };
+        //     _mesh.Faces[9] = new Face { A = 0, B = 4, C = 7 };
+        //     _mesh.Faces[10] = new Face { A = 4, B = 5, C = 6 };
+        //     _mesh.Faces[11] = new Face { A = 4, B = 6, C = 7 };
+        //
+        //     _camera.Position = new Vector3(0, 0, 10.0f);
+        //     _camera.Target = Vector3.Zero;
+        //
+        //     // Registering to the XAML rendering loop
+        //     CompositionTarget.Rendering += CompositionTarget_Rendering;
+        // }
+        //
+        // // Rendering loop handler
+        // private void CompositionTarget_Rendering(object sender, object e)
+        // {
+        //     device.Clear(0, 0, 0, 255);
+        //
+        //     // rotating slightly the cube during each frame rendered
+        //     _mesh.Rotation = new Vector3(_mesh.Rotation.X + 0.01f, _mesh.Rotation.Y + 0.01f, _mesh.Rotation.Z);
+        //
+        //     // Doing the various matrix operations
+        //     device.Render(_camera, _mesh);
+        //     // Flushing the back buffer into the front buffer
+        //     device.Present();
+        // }
+        
         private Device device;
-        Mesh _mesh = new Mesh("Cube", 8, 12);
-        Camera _camera = new Camera();
+        Mesh[] meshes;
+        Camera mera = new Camera();
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             // Choose the back buffer resolution here
             WriteableBitmap bmp = new WriteableBitmap(640, 480);
 
-            device = new Device(bmp);
-
             // Our Image XAML control
             frontBuffer.Source = bmp;
-
-            _mesh.Vertices[0] = new Vector3(-1, 1, 1);
-            _mesh.Vertices[1] = new Vector3(1, 1, 1);
-            _mesh.Vertices[2] = new Vector3(-1, -1, 1);
-            _mesh.Vertices[3] = new Vector3(1, -1, 1);
-            _mesh.Vertices[4] = new Vector3(-1, 1, -1);
-            _mesh.Vertices[5] = new Vector3(1, 1, -1);
-            _mesh.Vertices[6] = new Vector3(1, -1, -1);
-            _mesh.Vertices[7] = new Vector3(-1, -1, -1);
             
-            _mesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
-            _mesh.Faces[1] = new Face { A = 1, B = 2, C = 3 };
-            _mesh.Faces[2] = new Face { A = 1, B = 3, C = 6 };
-            _mesh.Faces[3] = new Face { A = 1, B = 5, C = 6 };
-            _mesh.Faces[4] = new Face { A = 0, B = 1, C = 4 };
-            _mesh.Faces[5] = new Face { A = 1, B = 4, C = 5 };
-            
-            _mesh.Faces[6] = new Face { A = 2, B = 3, C = 7 };
-            _mesh.Faces[7] = new Face { A = 3, B = 6, C = 7 };
-            _mesh.Faces[8] = new Face { A = 0, B = 2, C = 7 };
-            _mesh.Faces[9] = new Face { A = 0, B = 4, C = 7 };
-            _mesh.Faces[10] = new Face { A = 4, B = 5, C = 6 };
-            _mesh.Faces[11] = new Face { A = 4, B = 6, C = 7 };
-
-            _camera.Position = new Vector3(0, 0, 10.0f);
-            _camera.Target = Vector3.Zero;
+            device = new Device(bmp);
+            meshes = await device.LoadJSONFileAsync("monkey.babylon");
+            mera.Position = new Vector3(0, 0, 10.0f);
+            mera.Target = Vector3.Zero;
 
             // Registering to the XAML rendering loop
             CompositionTarget.Rendering += CompositionTarget_Rendering;
         }
 
         // Rendering loop handler
-        private void CompositionTarget_Rendering(object sender, object e)
+        void CompositionTarget_Rendering(object sender, object e)
         {
             device.Clear(0, 0, 0, 255);
 
-            // rotating slightly the cube during each frame rendered
-            _mesh.Rotation = new Vector3(_mesh.Rotation.X + 0.01f, _mesh.Rotation.Y + 0.01f, _mesh.Rotation.Z);
+            foreach (var mesh in meshes) {
+                // rotating slightly the meshes during each frame rendered
+                mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            }
 
             // Doing the various matrix operations
-            device.Render(_camera, _mesh);
+            device.Render(mera, meshes);
             // Flushing the back buffer into the front buffer
             device.Present();
         }
