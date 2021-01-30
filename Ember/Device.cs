eb3f43d7fd24b291cd7b15241d0122aa771f2357
billyeatcookies <billydevbusiness@gmail.com>
2021-01-30 15:34:34 +0000

    Fill Triangles

--- Ember/Device.cs
+++ Ember/Device.cs
@@ -10,14 +10,21 @@ namespace Ember
     public class Device
     {
         private byte[] backBuffer;
+        private readonly float[] depthBuffer;
         private WriteableBitmap bmp;
+        private readonly int renderWidth;
+        private readonly int renderHeight;
 
         public Device(WriteableBitmap bmp)
         {
             this.bmp = bmp;
+            renderWidth = bmp.PixelWidth;
+            renderHeight = bmp.PixelHeight;
+
             // the back buffer size is equal to the number of pixels to draw
             // on screen (width*height) * 4 (R,G,B & Alpha values). 
             backBuffer = new byte[bmp.PixelWidth * bmp.PixelHeight * 4];
+            depthBuffer = new float[bmp.PixelWidth * bmp.PixelHeight];
         }
 
         // This method is called to clear the back buffer with a specific color
@@ -61,7 +68,7 @@ namespace Ember
 
         // Project takes some 3D coordinates and transform them
         // in 2D coordinates using the transformation matrix
-        public Vector2 Project(Vector3 coord, Matrix transMat)
+        public Vector3 Project(Vector3 coord, Matrix transMat)
         {
             // transforming the coordinates
             var point = Vector3.TransformCoordinate(coord, transMat);
@@ -70,67 +77,194 @@ namespace Ember
             // from top left. We then need to transform them again to have x:0, y:0 on top left.
             var x = point.X * bmp.PixelWidth + bmp.PixelWidth / 2.0f;
             var y = -point.Y * bmp.PixelHeight + bmp.PixelHeight / 2.0f;
-            return (new Vector2(x, y));
+            return (new Vector3(x, y, point.Z));
         }
 
         // DrawPoint calls PutPixel but does the clipping operation before
-        public void DrawPoint(Vector2 point)
+        public void DrawPoint(Vector2 point, Color4 color)
         {
             // Clipping what's visible on screen
             if (point.X >= 0 && point.Y >= 0 && point.X < bmp.PixelWidth && point.Y < bmp.PixelHeight)
             {
-                // Drawing a yellow point
-                PutPixel((int)point.X, (int)point.Y, new Color4(1.0f, 1.0f, 0.0f, 1.0f));
+                // Drawing a point
+                PutPixel((int)point.X, (int)point.Y, color);
             }
         }
 
-        public void DrawLine(Vector2 point0, Vector2 point1)
+        // public void DrawLine(Vector2 point0, Vector2 point1)
+        // {
+        //     var dist = (point1 - point0).Length();
+        //
+        //     // If the distance between the 2 points is less than 2 pixels
+        //     // We're exiting
+        //     if (dist < 2)
+        //         return;
+        //
+        //     // Find the middle point between first & second point
+        //     Vector2 middlePoint = point0 + (point1 - point0)/2;
+        //     // We draw this point on screen
+        //     DrawPoint(middlePoint);
+        //     // Recursive algorithm launched between first & middle point
+        //     // and between middle & second point
+        //     DrawLine(point0, middlePoint);
+        //     DrawLine(middlePoint, point1);
+        // }
+
+        // public void DrawBLine(Vector2 point0, Vector2 point1)
+        // {
+        //     var x0 = (int)point0.X;
+        //     var y0 = (int)point0.Y;
+        //     var x1 = (int)point1.X;
+        //     var y1 = (int)point1.Y;
+        //     
+        //     var dx = Math.Abs(x1 - x0);
+        //     var dy = Math.Abs(y1 - y0);
+        //     var sx = (x0 < x1) ? 1 : -1;
+        //     var sy = (y0 < y1) ? 1 : -1;
+        //     var err = dx - dy;
+        //
+        //     while (true) {
+        //         DrawPoint(new Vector2(x0, y0));
+        //
+        //         if ((x0 == x1) && (y0 == y1))
+        //             break;
+        //         var e2 = 2 * err;
+        //         if (e2 > -dy) { err -= dy;
+        //             x0 += sx;
+        //         }
+        //         if (e2 < dx) { err += dx;
+        //             y0 += sy;
+        //         }
+        //     }
+        // }
+        
+        // drawing line between 2 points from left to right
+        // papb -> pcpd
+        // pa, pb, pc, pd must then be sorted before
+        private void ProcessScanLine(int y, Vector3 pa, Vector3 pb, Vector3 pc, Vector3 pd, Color4 color)
         {
-            var dist = (point1 - point0).Length();
-
-            // If the distance between the 2 points is less than 2 pixels
-            // We're exiting
-            if (dist < 2)
-                return;
-
-            // Find the middle point between first & second point
-            Vector2 middlePoint = point0 + (point1 - point0)/2;
-            // We draw this point on screen
-            DrawPoint(middlePoint);
-            // Recursive algorithm launched between first & middle point
-            // and between middle & second point
-            DrawLine(point0, middlePoint);
-            DrawLine(middlePoint, point1);
-        }
-
-        public void DrawBLine(Vector2 point0, Vector2 point1)
-        {
-            var x0 = (int)point0.X;
-            var y0 = (int)point0.Y;
-            var x1 = (int)point1.X;
-            var y1 = (int)point1.Y;
+            // Thanks to current Y, we can compute the gradient to compute others values like
+            // the starting X (sx) and ending X (ex) to draw between
+            // if pa.Y == pb.Y or pc.Y == pd.Y, gradient is forced to 1
+            var gradient1 = pa.Y != pb.Y ? (y - pa.Y) / (pb.Y - pa.Y) : 1;
+            var gradient2 = pc.Y != pd.Y ? (y - pc.Y) / (pd.Y - pc.Y) : 1;
             
-            var dx = Math.Abs(x1 - x0);
-            var dy = Math.Abs(y1 - y0);
-            var sx = (x0 < x1) ? 1 : -1;
-            var sy = (y0 < y1) ? 1 : -1;
-            var err = dx - dy;
+            int sx = (int)Interpolate(pa.X, pb.X, gradient1);
+            int ex = (int)Interpolate(pc.X, pd.X, gradient2);
 
-            while (true) {
-                DrawPoint(new Vector2(x0, y0));
+            // drawing a line from left (sx) to right (ex) 
+            for (var x = sx; x < ex; x++)
+            {
+                DrawPoint(new Vector2(x, y), color);
+            }
+        }
+        
+        // Clamping values to keep them between 0 and 1
+        private float Clamp(float value, float min = 0, float max = 1)
+        {
+            return Math.Max(min, Math.Min(value, max));
+        }
+        
+        // Interpolating the value between 2 vertices 
+        // min is the starting point, max the ending point
+        // and gradient the % between the 2 points
+        private float Interpolate(float min, float max, float gradient)
+        {
+            return min + (max - min) * Clamp(gradient);
+        }
 
-                if ((x0 == x1) && (y0 == y1))
-                    break;
-                var e2 = 2 * err;
-                if (e2 > -dy) { err -= dy;
-                    x0 += sx;
+        public void DrawTriangle(Vector3 p1, Vector3 p2, Vector3 p3, Color4 color)
+        {
+            // Sorting the points in order to always have this order on screen p1, p2 & p3
+            // with p1 always up (thus having the Y the lowest possible to be near the top screen)
+            // then p2 between p1 & p3
+            if (p1.Y > p2.Y)
+            {
+                var temp = p2;
+                p2 = p1;
+                p1 = temp;
+            }
+
+            if (p2.Y > p3.Y)
+            {
+                var temp = p2;
+                p2 = p3;
+                p3 = temp;
+            }
+
+            if (p1.Y > p2.Y)
+            {
+                var temp = p2;
+                p2 = p1;
+                p1 = temp;
+            }
+
+            // inverse slopes
+            float dP1P2, dP1P3;
+
+            // Computing inverse slopes
+            if (p2.Y - p1.Y > 0)
+                dP1P2 = (p2.X - p1.X) / (p2.Y - p1.Y);
+            else
+                dP1P2 = 0;
+
+            if (p3.Y - p1.Y > 0)
+                dP1P3 = (p3.X - p1.X) / (p3.Y - p1.Y);
+            else
+                dP1P3 = 0;
+
+            // First case where triangles are like that:
+            // P1
+            // -
+            // -- 
+            // - -
+            // -  -
+            // -   - P2
+            // -  -
+            // - -
+            // -
+            // P3
+            if (dP1P2 > dP1P3)
+            {
+                for (var y = (int)p1.Y; y <= (int)p3.Y; y++)
+                {
+                    if (y < p2.Y)
+                    {
+                        ProcessScanLine(y, p1, p3, p1, p2, color);
+                    }
+                    else
+                    {
+                        ProcessScanLine(y, p1, p3, p2, p3, color);
+                    }
                 }
-                if (e2 < dx) { err += dx;
-                    y0 += sy;
+            }
+            // First case where triangles are like that:
+            //       P1
+            //        -
+            //       -- 
+            //      - -
+            //     -  -
+            // P2 -   - 
+            //     -  -
+            //      - -
+            //        -
+            //       P3
+            else
+            {
+                for (var y = (int)p1.Y; y <= (int)p3.Y; y++)
+                {
+                    if (y < p2.Y)
+                    {
+                        ProcessScanLine(y, p1, p2, p1, p3, color);
+                    }
+                    else
+                    {
+                        ProcessScanLine(y, p2, p3, p1, p3, color);
+                    }
                 }
             }
         }
-
+        
         // The main method of the engine that re-compute each vertex projection
         // during each frame
         public void Render(Camera camera, params Mesh[] meshes)
@@ -162,7 +296,9 @@ namespace Ember
                 //     var point1 = Project(mesh.Vertices[i + 1], transformMatrix);
                 //     DrawLine(point0, point1);
                 // }
-
+                
+                var faceIndex = 0;
+                
                 foreach (var face in mesh.Faces)
                 {
                     var vertexA = mesh.Vertices[face.A];
@@ -173,9 +309,13 @@ namespace Ember
                     var pixelB = Project(vertexB, transformMatrix);
                     var pixelC = Project(vertexC, transformMatrix);
 
-                    DrawBLine(pixelA, pixelB);
-                    DrawBLine(pixelB, pixelC);
-                    DrawBLine(pixelC, pixelA);
+                    // DrawBLine(pixelA, pixelB);
+                    // DrawBLine(pixelB, pixelC);
+                    // DrawBLine(pixelC, pixelA);
+                    
+                    var color = 0.25f + (faceIndex % mesh.Faces.Length) * 0.75f / mesh.Faces.Length;
+                    DrawTriangle(pixelA, pixelB, pixelC, new Color4(color, color, color, 1));
+                    faceIndex++;
                 }
             }
         }
