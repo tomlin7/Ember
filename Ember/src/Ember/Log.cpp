2c2663637d8f23bacfca3ed7f96e7b1f5333f9ed
billyeatcookies <billydevbusiness@gmail.com>
2021-09-12 06:52:12 -0500

    Add Logging library to project
    - Added spdlog logging library to project

---
+++ Ember/src/Ember/Log.cpp
@@ -0,0 +1,11 @@
+#include "Log.h"
+
+namespace Ember {
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+	}
+}
---
+++ Ember/src/Ember/Log.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Ember {
+	class EMBER_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
