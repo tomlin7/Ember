c19dc9a67382ffa75587d6521b42f98425fb0873
billyeatcookies <billydevbusiness@gmail.com>
2021-09-15 01:47:36 -0500

    EventType, EventCategory MACROS

--- Ember/src/Ember/Events/Event.h
+++ Ember/src/Ember/Events/Event.h
@@ -32,9 +32,15 @@ namespace Ember {
 		EventCategoryMouseButton	= BIT(4),
 	};
 
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
 	class EMBER_API Event
 	{
-		friend class EventDispatcher;
+		// friend class EventDispatcher;
 	public:
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
