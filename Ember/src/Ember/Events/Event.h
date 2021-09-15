287d966ebc75326ffdfaf75559aa8795cc75ddeb
billyeatcookies <billydevbusiness@gmail.com>
2021-09-15 01:45:08 -0500

    Event Types [Event System]

--- Ember/src/Ember/Events/Event.h
+++ Ember/src/Ember/Events/Event.h
@@ -11,4 +11,14 @@ namespace Ember {
 	// immediately gets dispatched and must be dealt with right there an there.
 	// For the future, a better strategy might be to buffer events in an event
 	// bus and process them during the "event" part of the update stage.
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, 
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
 }
