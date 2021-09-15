01387ec4eadf1c495833f3a5741f0f4d45d81b6b
billyeatcookies <billydevbusiness@gmail.com>
2021-09-15 01:58:26 -0500

    Event Dispatcher

--- Ember/src/Ember/Events/Event.h
+++ Ember/src/Ember/Events/Event.h
@@ -54,4 +54,28 @@ namespace Ember {
 	protected:
 		bool m_Handled = false;
 	};
+
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event)
+		{}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)*m_Event);
+				return true;
+			}
+
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
 }
