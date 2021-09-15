62d90effe4cb71bdbca773cb5770f8fdc1818363
billyeatcookies <billydevbusiness@gmail.com>
2021-09-15 01:40:14 -0500

    Base Event class [Event System]

---
+++ Ember/src/Ember/Events/Event.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../Core.h"
+
+#include <string>
+#include <functional>
+
+
+namespace Ember {
+	// Events in Ember are currently blocking, meaning when an event occurs it
+	// immediately gets dispatched and must be dealt with right there an there.
+	// For the future, a better strategy might be to buffer events in an event
+	// bus and process them during the "event" part of the update stage.
+}
