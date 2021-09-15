f46d85458a753130e8afbcb7cf0bdd1d624870c1
billyeatcookies <billydevbusiness@gmail.com>
2021-09-15 01:45:53 -0500

    Event Category [Event System]

--- Ember/src/Ember/Core.h
+++ Ember/src/Ember/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error Ember only supports Windows!
 #endif
+
+#define BIT(x) (1 << x)
--- Ember/src/Ember/Events/Event.h
+++ Ember/src/Ember/Events/Event.h
@@ -21,4 +21,14 @@ namespace Ember {
 		MouseButtonPressed, MouseButtonReleased, 
 		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication	= BIT(0),
+		EventCategoryInput			= BIT(1),
+		EventCategoryKeyboard		= BIT(2),
+		EventCategoryMouse			= BIT(3),
+		EventCategoryMouseButton	= BIT(4),
+	};
 }
