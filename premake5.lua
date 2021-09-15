492c36e6d6ca13b5b32018b253ba322a726554b9
billyeatcookies <billydevbusiness@gmail.com>
2021-09-15 06:22:11 -0500

    Rewrite premake file to include GLFW

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,12 @@ workspace "Ember"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder {solution directory}
+IncludeDir = {}
+IncludeDir["GLFW"] = "Ember/vendor/GLFW/include"
+
+include "Ember/vendor/GLFW"
+
 project "Ember"
 	location "Ember"
 	kind "SharedLib"
@@ -30,7 +36,14 @@ project "Ember"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
 	}
 
 	filter "system:windows"
