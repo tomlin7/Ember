1e8f7fb2c9d14dbc0351fe7c7b7d921ffdd1a117
billyeatcookies <billydevbusiness@gmail.com>
2021-09-16 02:24:52 -0500

    Add LayerStack to application

--- Ember/src/Ember/Application.h
+++ Ember/src/Ember/Application.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "Core.h"
-#include "Events/Event.h"
+
+#include "Window.h"
+#include "Ember/LayerStack.h"
+#include "Ember/Events/Event.h"
 #include "Ember/Events/ApplicationEvent.h"
 
 #include "Window.h"
@@ -16,11 +19,15 @@ namespace Ember {
 		void Run();
 		
 		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 
 	// To be defined in CLIENT
