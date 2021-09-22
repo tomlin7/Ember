9eb84409c736f3473167c31edd5384a2ccc3e2f7
billyeatcookies <billydevbusiness@gmail.com>
2021-09-22 10:16:30 -0500

    - New `KeyTyped` Event

--- Ember/src/Ember/Events/KeyEvent.h
+++ Ember/src/Ember/Events/KeyEvent.h
@@ -52,4 +52,21 @@ namespace Ember
 
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
+
+	class EMBER_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
 }
