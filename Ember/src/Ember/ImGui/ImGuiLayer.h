63f1c5479191d6c5b7018df520561589813286b8
billyeatcookies <billydevbusiness@gmail.com>
2021-09-22 10:18:20 -0500

    On Event Functions     - `OnMouseButtonPressedEvent`     - `OnMouseButtonReleasedEvent`     - `OnMouseMovedEvent`     - `OnMouseScrolledEvent`     - `OnKeyPressedEvent`     - `OnKeyReleasedEvent`     - `OnKeyTypedEvent`     - OnWindowResizeEvent`

--- Ember/src/Ember/ImGui/ImGuiLayer.h
+++ Ember/src/Ember/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Ember/Layer.h"
 
+#include "Ember/Events/ApplicationEvent.h"
+#include "Ember/Events/KeyEvent.h"
+#include "Ember/Events/MouseEvent.h"
+
 namespace Ember {
 	class EMBER_API ImGuiLayer : public Layer
 	{
@@ -13,6 +17,15 @@ namespace Ember {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private: 
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
