8196eac8dd4b0c44d046b9b0342a1d1c12beb70f
billyeatcookies <billydevbusiness@gmail.com>
2021-09-23 07:39:09 -0500

    GetNativeWindow function exposing m_Window

--- Ember/src/Ember/Window.h
+++ Ember/src/Ember/Window.h
@@ -36,6 +36,8 @@ namespace Ember {
 		virtual void SetVSync(bool enabled) = 0;
 		virtual bool IsVSync() const = 0;
 
+		virtual void* GetNativeWindow() const = 0;
+
 		static Window* Create(const WindowProps& props = WindowProps());
 	};
 }
