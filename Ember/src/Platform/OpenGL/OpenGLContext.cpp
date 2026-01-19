327f8b80fc4553961e906f463b15fd3760548572
Billy <billydevbusiness@gmail.com>
2026-01-20 00:24:00 +0530

    feat: TRIANGLE!

--- Ember/src/Platform/OpenGL/OpenGLContext.cpp
+++ Ember/src/Platform/OpenGL/OpenGLContext.cpp
@@ -14,6 +14,10 @@ namespace Ember {
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
         EM_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+        EM_CORE_INFO("OpenGL Renderer: {0}", glGetString(GL_RENDERER));
+        EM_CORE_INFO("OpenGL Version: {0}", glGetString(GL_VERSION));
+        EM_CORE_INFO("OpenGL Shader Language Version: {0}", glGetString(GL_SHADING_LANGUAGE_VERSION));
     }
     void OpenGLContext::SwapBuffers() { glfwSwapBuffers(m_WindowHandle); }
 } // namespace Ember
